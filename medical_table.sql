2f1978a4d59a69fa5a37af51ff499e4096ad5b5d
tharushaherath00 <153209368+tharushaherath00@users.noreply.github.com>
2024-11-05 09:02:14 -0800

    updated Medical final

--- medical_table.sql
+++ medical_table.sql
@@ -12,13 +12,19 @@ FOREIGN KEY (course_ID) REFERENCES course(courseId) ON DELETE CASCADE ON UPDATE 
 
 INSERT INTO medical (Medical_Id, student_ID, course_ID, submit_Date, M_reason) 
 VALUES
-('MED01', 'TG/2022/0002', 'ICT1253','2024-10-03','Flu and fever'),
-('MED02', 'TG/2022/0002', 'ICT1253','2024-10-08','Fractured leg'),
-('MED03', 'TG/2022/0002', 'ICT1242','2024-10-11','Surgery recovery'),
-('MED04', 'TG/2022/0002', 'ICT1242','2024-10-18','Family emergency travel'),
-('MED05', 'TG/2022/0002', 'ICT1242','2024-10-24','Hospitalization for dengue'),
-('MED06', 'TG/2022/0005', 'BST1232','2024-10-11','Severe migraines'),
-('MED07', 'TG/2022/0005', 'BST1232','2024-10-18', 'Mental health consultation'),
-('MED08', 'TG/2022/0005', 'BST1232','2024-10-24', 'Family emergency travel'),
-('MED09', 'TG/2022/0009', 'ENT1242','2024-10-24', 'Allergic reaction treatment'),
-('MED10', 'TG/2022/0009', 'ENT1242','2024-10-25', 'Food poisoning');
+INSERT INTO medical (Medical_Id, student_ID, course_ID, Absent_Date, submit_Date, M_reason, Medical_Type) 
+VALUES
+('MED01', 'TG/2022/0002', 'ICT1253','2024-10-03','2024-10-08','Flu and fever','Attendance'),
+('MED02', 'TG/2022/0002', 'ICT1253','2024-10-08','2024-11-03','Fractured leg','Attendance'),
+('MED03', 'TG/2022/0002', 'ICT1242','2024-10-11','2024-10-13','Surgery recovery','Attendance'),
+('MED04', 'TG/2022/0002', 'ICT1242','2024-10-18','2024-10-23','Family emergency travel','Attendance'),
+('MED05', 'TG/2022/0002', 'ICT1242','2024-10-24','2024-10-27','Hospitalization for dengue','Attendance'),
+('MED06', 'TG/2022/0005', 'BST1232','2024-10-11','2024-10-30','Severe migraines','Attendance'),
+('MED07', 'TG/2022/0005', 'BST1232','2024-10-18','2024-11-03','Mental health consultation','Attendance'),
+('MED08', 'TG/2022/0005', 'BST1232','2024-10-24','2024-11-23','Family emergency travel','Attendance'),
+('MED09', 'TG/2022/0009', 'ENT1242','2024-10-24','2024-10-28','Allergic reaction treatment','Attendance'),
+('MED10', 'TG/2022/0009', 'ENT1242','2024-10-25','2024-10-26','Food poisoning','Attendance'),
+('MED11', 'TG/2022/0002', 'ICT1242','2024-10-26','2024-10-27','Allergic reaction','Exams'),
+('MED12', 'TG/2022/0005', 'BST1242','2024-10-27','2024-10-28','Migraine headache','Exams'),
+('MED13', 'TG/2022/0009', 'ENT1242','2024-10-28','2024-10-29','Fractured arm','Exams');
+
